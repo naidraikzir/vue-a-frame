285766d5a3efe8abd95a41a4facf65e551d51de8
Rizki Ardian <radeite8@gmail.com>
2017-10-02 23:36:09 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<div id="app">
+  <img class="vue" src="./assets/logo.png" alt="Vue.js PWA">
+  <hello-scene></hello-scene>
+</div>
+</template>
+
+<script>
+import HelloScene from './components/HelloScene'
+
+export default {
+  name: 'app',
+  components: {
+    HelloScene
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.vue {
+  max-height: 50px;
+  width: auto;
+  position: fixed;
+  bottom: 20px;
+  left: 20px;
+  z-index: 1;
+}
+</style>
---
+++ src/components/HelloScene.vue
@@ -0,0 +1,24 @@
+<template>
+<a-scene>
+  <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+  <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+  <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+  <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#43C877"></a-plane>
+  <a-sky color="#8ddfff"></a-sky>
+</a-scene>
+</template>
+
+<script>
+import 'aframe'
+
+export default {
+  name: 'HelloScene'
+}
+</script>
+
+<style>
+.fullscreen {
+  height: 100vh;
+  position: relative;
+}
+</style>
